4a4cbda4b6a174a6ed966f9828cbd3d83b532bbb
Henri Bragge <henri.bragge@esrlabs.com>
2017-07-10 22:44:21 +0200

    add and list
    Change-Id: I5e04bb5c074d3f3dbee9e68d31ea5777357e8c9a

--- subproject/app.js
+++ subproject/app.js
@@ -5,13 +5,50 @@ var express = require("express"),
     routes = require('./routes/index'),
     users = require('./routes/users');
 
-nano.db.create('subproject')
+nano.db.create('subproject');
+var sp = nano.use('subproject');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", function(req, res){
-    res.render("index");
+app.get("/", function(req, res) {
+    var _rows = [];
+    sp.list( function(err, body) {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        console.log(JSON.stringify(body.rows));
+
+        body.rows.forEach(function(doc, index) {
+            sp.get(doc.id, function(err, content) {
+                console.log(JSON.stringify(content));
+                _rows.push({
+                    id: doc.id,
+                    happy: content.happy ? 'ON' : 'OFF'
+                });
+                if (index === body.rows.length - 1){
+                    console.log(_rows);
+                    res.render("index", {rows: _rows});
+                }
+            });
+        });
+        console.log(_rows);
+    });
+});
+
+app.get("/add", function(req, res) {
+    console.log('step 0');
+    var name = req.query.name
+    var happy = req.query.happy === "on" ? true : false;
+    sp.insert({ happy: happy }, name, function(err, body, header) {
+        console.log('step 1');
+        if (err) {
+            console.log("ERROR:: insert ", err.message);
+            return;
+        }
+        console.log("inserted rabbit " + name + " " + happy);
+    });
 });
 
 app.listen(process.env.PORT, function(){
